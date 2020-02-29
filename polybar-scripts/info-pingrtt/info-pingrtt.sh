194199265dba47297562ed6fe9b13d46e508776b
onokatio <onokatio@gmail.com>
2020-02-29 23:10:08 +0900

    coloring pingrtt

--- polybar-scripts/info-pingrtt/info-pingrtt.sh
+++ polybar-scripts/info-pingrtt/info-pingrtt.sh
@@ -2,19 +2,21 @@
 
 HOST=1.1.1.1
 
-if ! ping=$(ping -n -c 1 -W 1 $HOST); then
-    echo "# ping failed"
-    return 1
+if ! ping=$(ping -n -c 1 -W 1 $HOST 2>&1); then
+    echo "ping unreachable"
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
     if [ "$rtt" -lt 50 ]; then
-        icon="%{F#3cb703}#%{F-}"
+        icon="%{F#3cb703}%{F-}"
+        rtt="%{F#3cb703}$rtt ms%{F-}"
     elif [ "$rtt" -lt 150 ]; then
-        icon="%{F#f9dd04}#%{F-}"
+        icon="%{F#f9dd04}%{F-}"
+        rtt="%{F#f9dd04}$rtt ms%{F-}"
     else
-        icon="%{F#d60606}#%{F-}"
+        icon="%{F#d60606}%{F-}"
+        rtt="%{F#d60606}$rtt ms%{F-}"
     fi
 
-    echo "$icon $rtt ms(v4)"
+    echo "$icon $rtt"
 fi
